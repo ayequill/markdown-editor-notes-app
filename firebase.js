23e789a662162c744aa30c8d7f0f9751d1f16512
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-22 10:31:11 +0000

    Using env variables

--- firebase.js
+++ firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+const API_KEY = import.meta.env.VITE_API_KEY
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCxeHCokoJw1CLz-orCzZ0oMPwEnOnVuLI",
+  apiKey: API_KEY,
   authDomain: "md-notes-app.firebaseapp.com",
   projectId: "md-notes-app",
   storageBucket: "md-notes-app.appspot.com",
@@ -18,8 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize database
-const db = getFirestore(app)
+export const db = getFirestore(app)
 
-const noteCollection = collection(db, "notes")
+export const noteCollection = collection(db, "notes")
 
-console.log(noteCollection)
